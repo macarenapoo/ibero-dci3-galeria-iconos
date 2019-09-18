5a4214acb97242e1fa49a2d4bdbbcd4916dbd7d4
Giselle <gigi25@live.com.mx>
2019-09-18 11:21:30 -0500

    Ediciones-Examen-Finales

--- script.js
+++ script.js
@@ -5,9 +5,45 @@ function mostrarIconoPrincipal(icono) {
   mainIcon.classList = "fas";
   mainIcon.classList.add(icono);
 }
+function validar() {
+  var nombre = document.getElementById("icon_input").value;
+  var validar = validar(texto);
+
+  function validar(texto) {
+      var n = nombre.length;
+      if (n >= 1) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+}
+
+function imprimirTodosLosIconos() {
+  document.getElementById("icons_list").innerHTML = "";
+  iconos.forEach(imprimirIcono);
+}
+
+function imprimirIcono(icono, index) {
+  var list = document.getElementById("icons_list");
+  list.insertAdjacentHTML('beforeend', `<li class="icon_container" onclick="mostrarIconoPrincipal('`+ icono +`')">
+<i class="close_icon fas fa-times" onclick="eliminarIcono(`+ index + `)"></i>
+<i class="icon fas `+ icono +`"></i>
+<div class="label">`+ icono +`</div>
+</li>`);
+}
+
+function agregarIcono() {
+  var icono = document.getElementById("icon_input").value;
+  iconos.push(icono);
+  imprimirTodosLosIconos();
+}
+
 
 function eliminarIcono() {
   event.stopPropagation();
   iconos.splice(index, 1);
   imprimirToodsLosIconos();
 }
+
+imprimirTodosLosIconos();
